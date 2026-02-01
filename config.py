65681ae97c023b35979990916c5bf4566c33b2a4
john <somarury@gmail.com>
2026-02-01 23:12:25 +0900

    perbaikan

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload
+    DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+    ENV = os.environ.get('FLASK_ENV', 'production')
